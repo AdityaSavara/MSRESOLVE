3507665a8a290f0267a79cf90771c56903f3a988
WattCL <40369568+WattCL@users.noreply.github.com>
2018-07-10 10:03:55 -0400

    Update test_3.py

--- UnitTests/ImportantAbcissaIdentifier/test_3.py
+++ UnitTests/ImportantAbcissaIdentifier/test_3.py
@@ -15,17 +15,17 @@ from MSRESOLVE import DistinguishedArrayChooser
 #2) getting the prefix (or suffix) arugument for check_results. This is just for the output filenames.
 suffix= UnitTesterSG.returnDigitFromFilename(__file__)
 #3) provide the input for the function you want to test (you can also import it from a pickled object, for example)
-data=numpy.array([[10],
+refMassFrags=numpy.array([[10],
                   [2],
                   [6],
                   [13]])
     
-data2=numpy.array([[1],
+correctionValues=numpy.array([[1],
                   [2],
                   [3],
                   [4]])
     
-data3=numpy.array([[10],
+rawSignals=numpy.array([[10],
                   [20],
                   [30],
                   [40]])
@@ -35,7 +35,7 @@ moleculesLikelihood = [1]
 sensitivityValues = [1]    
 
 #4) get the output of the function, which is what will typically be checked. 
-output = DistinguishedArrayChooser(data,data2,data3, moleculesLikelihood, sensitivityValues)
+output = DistinguishedArrayChooser(refMassFrags,correctionValues,rawSignals, moleculesLikelihood, sensitivityValues)
 #print(output)
 resultObj= output #, output[1], output[2]]  #You can alternatively populate resultObj with whatever you want, such as a list.
 #5) A string is also typically provided, but is an optional argument. You can provide whatever string you want.
