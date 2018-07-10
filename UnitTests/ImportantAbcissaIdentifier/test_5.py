5564ee805b972009317b3055894aa90453c9d8c2
WattCL <40369568+WattCL@users.noreply.github.com>
2018-07-10 10:07:58 -0400

    Update test_5.py

--- UnitTests/ImportantAbcissaIdentifier/test_5.py
+++ UnitTests/ImportantAbcissaIdentifier/test_5.py
@@ -15,27 +15,27 @@ from MSRESOLVE import DistinguishedArrayChooser
 #2) getting the prefix (or suffix) arugument for check_results. This is just for the output filenames.
 suffix= UnitTesterSG.returnDigitFromFilename(__file__)
 #3) provide the input for the function you want to test (you can also import it from a pickled object, for example)
-data=numpy.array([[10, 3, 2],
-                  [2, 1, 4],
-                  [6, 11, 7],
-                  [13, 5, 4]])
+refMassFrags=numpy.array([[10, 3, 2],
+                          [2, 1, 4],
+                          [6, 11, 7],
+                          [13, 5, 4]])
     
-data2=numpy.array([[1, 1, 1],
-                  [2, 2, 2],
-                  [3, 3, 3],
-                  [4, 4, 4]])
+correctionValues=numpy.array([[1, 1, 1],
+                              [2, 2, 2],
+                              [3, 3, 3],
+                              [4, 4, 4]])
     
-data3=numpy.array([[10],
-                  [20],
-                  [30],
-                  [40]])
+rawSignals=numpy.array([[10],
+                        [20],
+                        [30],
+                        [40]])
                   
 moleculesLikelihood = [1,0.5,0.75]
     
 sensitivityValues = []    
 
 #4) get the output of the function, which is what will typically be checked. 
-output = DistinguishedArrayChooser(data,data2,data3, moleculesLikelihood, sensitivityValues)
+output = DistinguishedArrayChooser(rawSignals,correctionValues,rawSignals, moleculesLikelihood, sensitivityValues)
 #print(output)
 resultObj= output #, output[1], output[2]]  #You can alternatively populate resultObj with whatever you want, such as a list.
 #5) A string is also typically provided, but is an optional argument. You can provide whatever string you want.
