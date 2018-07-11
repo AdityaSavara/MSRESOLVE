15b29e8a184747bc35b202f85cf4ed5171503133
AdityaSavara <39929571+AdityaSavara@users.noreply.github.com>
2018-07-11 18:23:52 -0400

    pytest Driver

---
+++ UnitTests/pytestDriver.py
@@ -0,0 +1,16 @@
+import os
+
+#in Python, the listdir command  returns files and directories (like typeing in "dir").
+listOfDirectoriesAndFiles = os.listdir(".")
+
+#Below is going to become a list of directories only in the next loop.
+directoryList = []
+for elem in listOfDirectoriesAndFiles:
+    if os.path.isdir(elem) == True:
+        directoryList.append(elem)
+
+#This loop goes into each directories, runs the specified command, and comes back.
+for directory in directoryList:
+    os.chdir(directory)
+    os.system("pytest")
+    os.chdir("..")
