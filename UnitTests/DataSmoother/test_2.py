c835eb764f22ef884239f543a5896b2b28aacc5d
aroger34 <39954833+aroger34@users.noreply.github.com>
2018-07-13 08:30:28 -0400

    Add files via upload
    Removed returnDigitFromFilename in test files

--- UnitTests/DataSmoother/test_2.py
+++ UnitTests/DataSmoother/test_2.py
@@ -9,22 +9,13 @@ import sys
 sys.path.insert(1, "..\\lib")
 #importing the functions from UnitTesterSG module
 import UnitTesterSG as ut
-#extracting the digit from the file name to use as prefix/suffix in check_results
-def return_digit_from_filename():
-    import os
-    filename=os.path.basename(__file__)
-    import re
-    listOfNumbers=re.findall('\d+',filename)
-    #the digit is the first element in list of numbers
-    extractedDigit=listOfNumbers[0]
-    return extractedDigit
 
 #BELOW ARE THE LINES INTENDED TO BE CHANGED BY THE USER	
 #1) import the function whose results need to be checked
 from XYYYDataFunctionsSG import DataSmoother
 import numpy as np
 #2) getting the prefix (or suffix) arugument for check_results. This is just for the output filenames.
-suffix=return_digit_from_filename()
+suffix=ut.returnDigitFromFilename(__file__)
 #3) provide the input for the function you want to test (you can also import it from a pickled object, for example)
 data = np.array([[0., 1.5, 3.4, 5., 6.5, 7.1, 7.5],
                  [0.1, 0.2, 0.45, .45, .55, .65, 0.69]])
