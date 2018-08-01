c81cc7d1a1be607d9c2f40ec355bb67cf15bc699
akraetz <39954487+akraetz@users.noreply.github.com>
2018-08-01 15:57:43 -0400

    Add files via upload

--- UnitTests/RoughUniquenessCheck/test_2.py
+++ UnitTests/RoughUniquenessCheck/test_2.py
@@ -11,14 +11,14 @@ import sys
 sys.path.insert(1, "..\\lib")
 sys.path.insert(1, "..")
 sys.path.insert(1, "..\..")
-import UnitTesterSG 
-import numpy
+import UnitTesterSG as ut
 
 #BELOW ARE THE LINES INTENDED TO BE CHANGED BY THE USER	
 #1) import the function whose results need to be checked
 import roughUniquenessCheck as ruc
 #2) getting the prefix (or suffix) arugument for check_results. This is just for the output filenames.
-suffix= UnitTesterSG.returnDigitFromFilename(__file__)
+suffix= ut.returnDigitFromFilename(__file__)
+prefix=''
 #3) provide the input for the function you want to test (you can also import it from a pickled object, for example)
 massFragCombinations=([1,2,3,4,5],[2,1,3,4,5],[3,4,5,6,7],[9,8,7,6,5],[3,4,5,1,2])
 
@@ -30,23 +30,18 @@ keep_N_ValuesInRoughUniquenessCheck=4
 
 #4) get the output of the function, which is what will typically be checked.
 for counter, massFragCombination in enumerate(massFragCombinations):
- [topRoughUniquenessCheckList,valueStoredInRUTopList] = ruc.roughUniquenessCheck(rowSumsList[counter], topRoughUniquenessCheckList, keep_N_ValuesInRoughUniquenessCheck, massFragCombination)
+    [topRoughUniquenessCheckList,valueStoredInRUTopList] = ruc.roughUniquenessCheck(rowSumsList[counter], topRoughUniquenessCheckList, keep_N_ValuesInRoughUniquenessCheck, massFragCombination)
 #print(output)
 resultObj= [topRoughUniquenessCheckList,valueStoredInRUTopList] #, output[1], output[2]]  #You can alternatively populate resultObj with whatever you want, such as a list.
 #5) A string is also typically provided, but is an optional argument. You can provide whatever string you want.
 resultStr= str(resultObj)
 #6) Checking the result of the function using check_results. In this case the result is sumList1 object. 
 
-#run the Unit Tester
-def test_Run(allowOverwrite = False):
-    #if the user wants to be able to change what the saved outputs are
-    if allowOverwrite:
-        #This function call is used when this test is run solo as well as by UnitTesterSG
-        UnitTesterSG.check_results(resultObj, resultStr, prefix = '', suffix=suffix)
-    #this option allows pytest to call the function
-    if not allowOverwrite: 
-        #this assert statement is required for the pytest module 
-        assert UnitTesterSG.check_results(resultObj, resultStr, prefix = '', suffix=suffix, allowOverwrite = False) == True
+#this is so that pytest can do UnitTesterSG tests.
+def test_pytest(): #note that it cannot have any required arguments for pytest to use it, and that it is using variables that are defined above in the module.
+    ut.doTest(resultObj, resultStr, prefix=prefix,suffix=suffix, allowOverwrite = False)
+    
     
 if __name__ == "__main__":
-   test_Run(allowOverwrite = True)
+   #This is the normal way of using the UnitTesterSG module, and will be run by UnitTesterSG or by running this test file by itself.
+   ut.doTest(resultObj, resultStr, prefix=prefix,suffix=suffix, allowOverwrite = True)
