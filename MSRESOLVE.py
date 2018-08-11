8f52f178f3e56074c263aaa44cbdbf3a7d967c3b
AdityaSavara <39929571+AdityaSavara@users.noreply.github.com>
2018-08-11 09:12:36 -0400

    Update MSRESOLVE.py

--- MSRESOLVE.py
+++ MSRESOLVE.py
@@ -29,7 +29,8 @@ def passesRowsSumChecks(rowSumsList, massFragCombination, allOverlappingPatterns
     passesRowsSumChecks=True#Initialize return variable as true
     if all(rowSum==numberOfMassFragments for rowSum in rowSumsList): #Check if the array passed to it is full. If it is full, it appends to the allOverlapping patterns list
         allOverlappingPatterns.append(massFragCombination)###May be an error with the use of allOverlapping patterns
-        print(allOverlappingPatterns)
+        #FIXME: #TODO: should be printed to a file.
+	print(allOverlappingPatterns)
         passesRowsSumChecks=False
     elif 0 in rowSumsList: #Check if any row is entirely full of zeros
         passesRowsSumChecks=False
