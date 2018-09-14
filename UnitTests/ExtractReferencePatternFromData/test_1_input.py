87645b0156adff8f6cbbd5abea28365f6eb3486a
aroger34 <39954833+aroger34@users.noreply.github.com>
2018-09-14 14:56:51 -0400

    Add files via upload

--- UnitTests/ExtractReferencePatternFromData/test_1_input.py
+++ UnitTests/ExtractReferencePatternFromData/test_1_input.py
@@ -6,7 +6,7 @@ if os.path.basename(__file__) != "DefaultUserInput.py":
 
 #USER INPUT FILE
 #//Input Files//
-referenceFileName = 'AcetaldehydeNISTRefMixed2.csv' #enter the file name of the file containing reference information
+referenceFileNameList = 'AcetaldehydeNISTRefMixed2.csv' #enter the file name of the file containing reference information
 form = 'xyyy'	#form is either 'xyyy' or 'xyxy'
 referencePatternTimeRanges = [] #Leave empty if not using reference pattern time chooser []
 collectedFileName = '2-CrotAcetExp#2Truncated.csv'	#enter the file name with raw mass spectrometer data
@@ -40,7 +40,7 @@ timeRangeFinish = 900	#finish time (-int)
 #//Chosen Molecules
 #To choose only specific molecules to solve, input in a list of strings  below
 specificMolecules = 'no'
-chosenMolecules = ['CO', 'CO2', 'H2O']
+chosenMoleculesNames = ['CO', 'CO2', 'H2O']
 
 #//Chosen Mass Fragments//
 #To choose only specific mass fragments from collected data, input below:
@@ -96,7 +96,7 @@ dataUpperBound = []
 dataRangeSpecifierYorN = 'no' 
 signalOrConcentrationRange = 'signal'	#'signal' or 'concentration'
 csvFile = 'yes'	#'yes' or 'no'
-moleculesRange = []
+moleculesToRestrict = []
 csvFileName = 'rangestemplate.csv'
 #NOTE: The increment choice of the user is then possibly overridden based on 
 # the values of maxPermutations (the number of molecules and increments might 
