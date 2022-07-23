f14daa7444670b903b3a5173765af7ba9bd3f5aa
AdityaSavara <39929571+AdityaSavara@users.noreply.github.com>
2022-07-22 21:38:14 -0400

    userinput comment improvement

--- DefaultUserInput.py
+++ DefaultUserInput.py
@@ -189,8 +189,8 @@ UserChoices['extractReferencePatternFromDataOption']['rpcMoleculesToChangeMF'] =
 UserChoices['minimalReferenceValue'] = {} #initialize the minimalReferenceValue container
 # if you want to exclude tiny fragmentation peaks
 UserChoices['minimalReferenceValue']['on'] = 'no'
-UserChoices['minimalReferenceValue']['referenceValueThreshold'] = [2.0]  #typical values are between 1 and 5. This approximates smaller fragmentation peaks as '0', though implicitSLS will correct for the approximation.
-UserChoices['minimalReferenceValue']['referenceSignificantFragmentThresholds'] = [6.0] #typical values are between 5 and 50. This setting causes MSRESOLVE to favor larger intensity reference peaks (above the number provided) during solving.
+UserChoices['minimalReferenceValue']['referenceValueThreshold'] = [5.0]  #typical values are between 1 and 5. Can be a list (one value for each molecule) or a single value across all molecules. The list case has not been tested with all features. This approximates smaller fragmentation peaks as '0', though implicitSLS will correct for the approximation.
+UserChoices['minimalReferenceValue']['referenceSignificantFragmentThresholds'] = [6.0] #typical values are between 5 and 50. Can be a list (one value for each molecule) or a single value across all molecules. The list case has not been tested with all features. This setting causes MSRESOLVE to favor larger intensity reference peaks (above the number provided) during solving.
 
 #//Data Threshold Filter//
 UserChoices['lowerBoundThresholdChooser'] = {} #initialize the lowerBoundThresholdChooser container
