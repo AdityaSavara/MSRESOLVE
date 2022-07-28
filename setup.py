3d793c7cd2b90a91f85fbc06adc0238f1e311208
AdityaSavara <39929571+AdityaSavara@users.noreply.github.com>
2022-07-28 19:05:50 -0400

    Update setup.py
    all unit tests passing

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ URL = 'https://github.com/AdityaSavara/MSRESOLVESG'
 EMAIL = 'AditySavara2008@u.northwestern.edu'  
 AUTHOR = 'Aditya Savara'
 REQUIRES_PYTHON = '>=3.5.0'
-VERSION = '44.9.0'
+VERSION = '44.9.2'
 LICENSE = 'BSD-3-Clause'
 
 # What packages are required for this module to be executed?
